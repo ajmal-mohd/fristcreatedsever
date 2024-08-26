e8aa67fe205f1e20c696ddcb1010bef4e5aceada
ajmalpk <ajmalpk016@gmail.com>
2024-08-26 11:55:46 +0530

    server second add

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 
+//  1st model serverCREATEtype
+
+// const http= require("http")
+
+// const server=http.createServer((req,res)=>{
+
+//   res.write("hello WORLD")
+//   res.end()
+
+// }).listen(3001,()=>{
+
+//     console.log("server runnin on");
+// })
+
+
+
+// standerdtype
+
 const http= require("http")
 
-const server=http.createServer((req,res)=>{
+const server= http.createServer((req,res)=>{
 
-  res.write("hello WORLD")
-  res.end()
-
-}).listen(3001,()=>{
-
-    console.log("server runnin on");
+    res.write("hello my guys")
+    res.end();
+     
 })
 
+const port= process.env.PORT||3001
+
+server.listen(port,()=>{
+    console.log("sever running on");
+})
+
+
+
+
+
+
+
+ 
